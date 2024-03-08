055354cc065798286bb8b264fe46cfc345261cd0
Aleksandr Pulin <ecomeel@gmail.com>
2024-03-08 23:05:19 +0300

    build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  buildDir: 'doc',
   devtools: { enabled: true },
   css: [
     { src: '/assets/styles/commons.scss', lang: 'scss' }
